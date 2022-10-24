98ae1bb87a23793fe6909b3d0a5f7db926b8ffec
Moehid23 <muthadoabdul23@gmail.com>
2022-10-24 11:40:13 +0700

    Menambahkan Jquary

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
-$('.page-scroll').on('click',function(){
-    //ambil isi href
-    var tujuan= $(this).attr('href');
-    //tangkap element bersangkutan
-    var elementTujuan = $(tujuan);
-    //pindahkan scrollnya
-    $('body').animate({
-        scrollTop: elementTujuan.offset().top - 50
-    }, 1000);
+//event pada saat link diklik
+$('.page-scroll').on('click',function(e){
 
+//ambil isi href
+var tujuan= $(this).attr('href');
 
-})();
+//ambil element ybs
+var elementTujuan = $(tujuan);
+
+//pindahkan scroll
+$('html,body').animate({
+    scrollTop: elementTujuan.offset().top -50
+
+},1250, 'easeInOutExpo');
+e.preventDefault();
+
+});
