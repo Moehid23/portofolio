c9347375d746cb2db717547c7898d1d6fb1561e3
Moehid23 <muthadoabdul23@gmail.com>
2022-10-25 09:27:32 +0700

    menambahkan paralax effect

--- js/script.js
+++ js/script.js
@@ -15,3 +15,32 @@ $('html,body').animate({
 e.preventDefault();
 
 });
+//about
+$(window).on('load', function(){
+    $('.pkiri').addClass('pmuncul')
+    $('.pkanan').addClass('pmuncul')
+});
+//paralax jumbotron
+$(window).scroll(function(){
+    var wScroll = $(this).scrollTop();
+    
+    $('.jumbotron img').css({
+        'transform' : 'translate(0px, '+ wScroll/4 +'%)'
+    });
+
+    $('.jumbotron h1').css({
+        'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+    });
+    $('.jumbotron p').css({
+        'transform' : 'translate(0px, '+ wScroll/1.3 +'%)'
+    });
+
+    //portofolio
+    if(wScroll > $('.portofolio').offset().top -250){
+        $('.portofolio .thumbnail').each(function(i){
+            setTimeout(function(){
+                $('.portofolio .thumbnail').eq(i).addClass('muncul');
+            },300 *(i+1) );
+        });
+    }
+});
